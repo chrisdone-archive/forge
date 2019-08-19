ad5c3ab475276aa211dc7b671e4e21c8851c7081
Chris Done <chrisdone@gmail.com>
2019-08-19 17:28:56 +0100

    Fill in test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 import Forge.Generate
 import Forge.Lucid
+import Test.Hspec
+import Data.Functor.Identity
+import Lucid
+import Forge.Internal.Types
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec
+    (describe
+       "Lucid"
+       (do it
+             "View"
+             (shouldBe
+                (renderText
+                   (runIdentity
+                      (view
+                         @Lucid
+                         ((,) <$> FieldForm (pure IntegerField) <*>
+                          FieldForm (pure TextField)))))
+                "<input name=\"/l/m/\" type=\"number\"><input name=\"/r/\">")))
